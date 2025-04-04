a0cf786605a394ec2b5b063fbb34879564e123db
ZUGA <38326450+JanZugic@users.noreply.github.com>
2025-04-04 02:26:40 +0200

    Configuring git tracking

---
+++ Task01/Flower.h
@@ -0,0 +1,18 @@
+#pragma once
+class Flower
+{
+private:
+	sf::Window& window;	//address of sml window object
+public:
+		
+	Flower(sf::Window& window)
+	{
+		
+
+
+	};
+	//cosntructor with the variable
+};
+
+
+
